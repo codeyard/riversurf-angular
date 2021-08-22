6d6d47f618b134620eaf3735be5ceb8467f4e9fc
A.Messerli <messa4@bfh.ch>
2021-08-22 16:13:19 +0200

    Updated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDividerModule} from "@angular/material/divider";
 import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import { HomeComponent } from './views/home/home.component';
+import {HomeComponent} from './views/home/home.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatChipsModule} from "@angular/material/chips";
-import { EventCardComponent } from './views/home/event-card/event-card.component';
+import {EventCardComponent} from './views/home/event-card/event-card.component';
 import {MatExpansionModule} from "@angular/material/expansion";
-import {RiderCardComponent} from './common/rider-card/rider-card.component';
-import {EventComponent} from './views/event/event.component';
-import {RidersComponent} from './views/riders/riders.component';
-import {RiderProfileComponent} from './views/riders/rider-profile/rider-profile.component';
-import {RegisterComponent} from './views/register/register.component';
-import {LoginComponent} from './views/login/login.component';
-import {CompetitionComponent} from './views/competition/competition.component';
-import {ErrorComponent} from './views/error/error.component';
-import {HeaderComponent} from './common/header/header.component';
-import {RiderCardComponent} from './common/rider-card/rider-card.component';
-import {EventComponent} from './views/event/event.component';
-import {RidersComponent} from './views/riders/riders.component';
-import {RiderProfileComponent} from './views/riders/rider-profile/rider-profile.component';
-import {LoginComponent} from './views/login/login.component';
-import {CompetitionComponent} from './views/competition/competition.component';
-import {ErrorComponent} from './views/error/error.component';
-import {HeaderComponent} from './common/header/header.component';
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatMenuModule} from "@angular/material/menu";
-import {NavigationComponent} from './common/header/navigation/navigation.component';
 import {WeatherComponent} from "./components/weather/weather.component";
 import {MatGridListModule} from "@angular/material/grid-list";
-import {WeatherService} from "./components/weather/weather.service";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {NavigationComponent} from './common/header/navigation/navigation.component';
 import {SignupFormComponent} from './common/signup-form/signup-form.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -47,7 +28,14 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatInputModule} from "@angular/material/input";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatSnackBarModule} from "@angular/material/snack-bar";
-
+import {RiderCardComponent} from "./common/rider-card/rider-card.component";
+import {EventComponent} from "./views/event/event.component";
+import {ErrorComponent} from "./views/error/error.component";
+import {RiderProfileComponent} from "./views/riders/rider-profile/rider-profile.component";
+import {LoginComponent} from "./views/login/login.component";
+import {RidersComponent} from "./views/riders/riders.component";
+import {CompetitionComponent} from "./views/competition/competition.component";
+import {HeaderComponent} from "./common/header/header.component";
 
 @NgModule({
     declarations: [
@@ -58,28 +46,13 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
         EventComponent,
         RidersComponent,
         RiderProfileComponent,
-        RegisterComponent,
         LoginComponent,
         CompetitionComponent,
         ErrorComponent,
-        HeaderComponent,
         NavigationComponent,
-        WeatherComponent
-    ],
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        EventCardComponent,
-        RiderCardComponent,
-        EventComponent,
-        RidersComponent,
-        RiderProfileComponent,
-        LoginComponent,
-        CompetitionComponent,
-        ErrorComponent,
-        HeaderComponent,
-        NavigationComponent,
-        SignupFormComponent
+        SignupFormComponent,
+        WeatherComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
@@ -88,19 +61,16 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
         MatDividerModule,
         MatIconModule,
         MatToolbarModule,
-        MatButtonModule,
         MatProgressSpinnerModule,
         MatCardModule,
         MatChipsModule,
         MatExpansionModule,
         MatAutocompleteModule,
-        MatMenuModule,
         MatGridListModule,
-        HttpClientModule
+        HttpClientModule,
         FormsModule,
         ReactiveFormsModule,
         MatMenuModule,
-        MatFormFieldModule,
         MatFormFieldModule,
         MatInputModule,
         MatSelectModule,
@@ -109,7 +79,8 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
         MatTabsModule,
         MatSnackBarModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

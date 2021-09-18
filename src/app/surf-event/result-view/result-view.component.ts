adb139bba5137a1166681799ed4608700cd3d5f1
Stalder Marius <stalm12@bfh.ch>
2021-09-18 18:53:08 +0200

    feat(69): simple rounded path solution

--- src/app/surf-event/result-view/result-view.component.ts
+++ src/app/surf-event/result-view/result-view.component.ts
@@ -13,7 +13,8 @@ import {not} from "rxjs/internal-compatibility";
 
 export interface Line {
     source: Point,
-    target: Point
+    target: Point,
+    path: string,
 }
 
 interface Point {
@@ -36,6 +37,10 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
 
     RIDER_WIDTH = 250;
     RIDER_HEIGHT = 25;
+
+    CURVE_RADIUS = 20;
+    VARIANZ = 0;
+    VARIANZ_OFFSET = 0;
 
 
     constructor(private cd: ChangeDetectorRef) {
@@ -68,7 +73,7 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
 
     getLine() {
         let relevantResultsForPoints: Result[][] = [];
-        for (let i = 0; i < this.competition.rounds.length; i+=2) {
+        for (let i = 0; i < this.competition.rounds.length; i += 2) {
             const temp = this.competition.rounds[i].heats.map(heat => heat.results.map(result => result));
             const resultsFirst = temp.reduce((acc, val) => acc.concat(val), []);
             let resultSucceeding: any[] = []
@@ -82,25 +87,33 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
 
         const resultsWithLine = relevantResultsForPoints.reduce((acc, val) => acc.concat(val), []);
 
-        for(const result of resultsWithLine) {
+        for (const result of resultsWithLine) {
             let points: Point[] = [];
             this.results.forEach(resultElementRef => {
-                if(resultElementRef.riderId === result.riderId) {
-                    if(resultElementRef.roundNumber === 0) {
-                        const rightPoint = {x: resultElementRef.elementRef.nativeElement.offsetLeft + this.RIDER_WIDTH,
-                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT}
+                if (resultElementRef.riderId === result.riderId) {
+                    if (resultElementRef.roundNumber === 0) {
+                        const rightPoint = {
+                            x: resultElementRef.elementRef.nativeElement.offsetLeft + this.RIDER_WIDTH,
+                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT
+                        }
                         points.push(rightPoint)
                         this.points.push(rightPoint)
-                    } else if(resultElementRef.roundNumber === (this.competition.rounds.length - 1)) {
-                        const leftPoint = {x: resultElementRef.elementRef.nativeElement.offsetLeft,
-                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT}
+                    } else if (resultElementRef.roundNumber === (this.competition.rounds.length - 1)) {
+                        const leftPoint = {
+                            x: resultElementRef.elementRef.nativeElement.offsetLeft,
+                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT
+                        }
                         points.push(leftPoint)
                         this.points.push(leftPoint)
                     } else {
-                        const leftPoint = {x: resultElementRef.elementRef.nativeElement.offsetLeft,
-                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT};
-                        const rightPoint = {x: resultElementRef.elementRef.nativeElement.offsetLeft + this.RIDER_WIDTH,
-                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT}
+                        const leftPoint = {
+                            x: resultElementRef.elementRef.nativeElement.offsetLeft,
+                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT
+                        };
+                        const rightPoint = {
+                            x: resultElementRef.elementRef.nativeElement.offsetLeft + this.RIDER_WIDTH,
+                            y: resultElementRef.elementRef.nativeElement.offsetTop + this.RIDER_HEIGHT
+                        }
                         points.push(leftPoint)
                         points.push(rightPoint)
                         this.points.push(leftPoint)
@@ -109,18 +122,14 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
                 }
             })
 
-            for(let i = 0; i < points.length; i+=2) {
+            for (let i = 0; i < points.length; i += 2) {
                 try {
+                    let {a, b} = this.extractPoints(points, i);
+                    let path = this.calculatePath(a, b);
                     this.lines.push({
-                        source: {
-                            x: points[i].x,
-                            y: points[i].y,
-
-                        },
-                        target: {
-                            x: points[i + 1].x,
-                            y: points[i + 1].y
-                        }
+                        source: a,
+                        target: b,
+                        path: path
                     })
                 } catch (notLineException) {
                     // EMPTY AS NO NEED TO ADD LINE
@@ -130,8 +139,35 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
         }
     }
 
+    private extractPoints(points: Point[], i: number): { a: Point, b: Point } {
+        return {
+            a: {
+                x: points[i].x,
+                y: points[i].y
+            },
+            b: {
+                x: points[i + 1].x,
+                y: points[i + 1].y
+            }
+
+        }
+    }
 
 
+    private calculatePath(a: Point, b: Point) {
+
+        const deltaX = b.x - a.x;
+        const varianz = Math.random() * this.VARIANZ + this.VARIANZ_OFFSET;
+        const middleX = a.x + (deltaX / 2) + varianz;
+        console.log("middleX:", middleX);
+        let deltaY = b.y - a.y;
+        let middleY = a.y + (deltaY / 2);
+        let signY = Math.sign(deltaY)
+        // TODO: funktionert momentan nur für horizontale verbindungen, für vertikale, muss wohl auch noch ein signX eingesetzt werden
+        // M 0 0 L 1 0 Q 2 0 2 1 L 2 2 L 2 3 Q 2 4 3 4 L 4 4
+        let path = `M ${a.x} ${a.y}, L ${middleX - this.CURVE_RADIUS} ${a.y}, Q ${middleX} ${a.y} ${middleX} ${a.y + signY * this.CURVE_RADIUS}, L ${middleX} ${middleY}, L ${middleX} ${b.y - signY * this.CURVE_RADIUS}, Q ${middleX} ${b.y} ${middleX + this.CURVE_RADIUS} ${b.y}, L ${b.x} ${b.y}`;
+        return path;
+    }
 }
 
 
@@ -191,7 +227,7 @@ const exampleComp: Competition = {
                         color: 3,
                         value: 0
                     }]
-                },  {
+                }, {
                     id: 2,
                     riders: ["6132710cfc13ae15b300000c", "6132710cfc13ae15b300000d", "6132710cfc13ae15b300000e", "6132710cfc13ae15b300000f"],
                     state: 'idle',

dadca258a9a5a22bff595f1d44590fc8b30708b3
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-09-18 13:19:47 +0200

    making this shit work.

--- src/app/surf-event/result-view/result-view.component.ts
+++ src/app/surf-event/result-view/result-view.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/core';
 import {Competition, Heat, Result} from "../../core/models/competition.model";
 import {RiderResultComponent} from "../surf-event/competition/round/rider-result/rider-result.component";
+import {not} from "rxjs/internal-compatibility";
 
 export interface Line {
     source: Point,
@@ -34,7 +35,7 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
     points: Point[] = []
 
     RIDER_WIDTH = 250;
-    RIDER_HEIGHT = 80;
+    RIDER_HEIGHT = 25;
 
 
     constructor(private cd: ChangeDetectorRef) {
@@ -48,10 +49,8 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
             case 'finished':
             case 'completed':
                 return 'finished';
-
             case 'running':
                 return 'surfing';
-
             default:
                 return 'assigned';
         }
@@ -69,7 +68,7 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
 
     getLine() {
         let relevantResultsForPoints: Result[][] = [];
-        for (let i = 0; i < this.competition.rounds.length; i++) {
+        for (let i = 0; i < this.competition.rounds.length; i+=2) {
             const temp = this.competition.rounds[i].heats.map(heat => heat.results.map(result => result));
             const resultsFirst = temp.reduce((acc, val) => acc.concat(val), []);
             let resultSucceeding: any[] = []
@@ -82,6 +81,7 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
         }
 
         const resultsWithLine = relevantResultsForPoints.reduce((acc, val) => acc.concat(val), []);
+        console.log(resultsWithLine)
 
         for(const result of resultsWithLine) {
             let points: Point[] = [];
@@ -108,22 +108,27 @@ export class ResultViewComponent implements OnInit, AfterViewInit, AfterViewChec
                         this.points.push(rightPoint)
                     }
                 }
-
             })
-            this.lines.push({
-                source: {
-                    x: points[0].x,
-                    y: points[0].y,
 
-                },
-                target: {
-                    x: points[1].x,
-                    y: points[1].y
+            for(let i = 0; i < points.length; i+=2) {
+                try {
+                    this.lines.push({
+                        source: {
+                            x: points[i].x,
+                            y: points[i].y,
+
+                        },
+                        target: {
+                            x: points[i + 1].x,
+                            y: points[i + 1].y
+                        }
+                    })
+                } catch (notLineException) {
+                    console.log(notLineException)
                 }
-            })
-        }
+            }
 
-        console.log(this.points)
+        }
     }
 
 
@@ -317,6 +322,72 @@ const exampleComp: Competition = {
         },
         {
             id: 2,
+            riders: ["6132710cfc13ae15b3000001", "6132710cfc13ae15b3000002", "6132710cfc13ae15b3000003", "6132710cfc13ae15b3000004", "6132710cfc13ae15b3000005", "6132710cfc13ae15b3000006", "6132710cfc13ae15b3000007", "6132710cfc13ae15b3000008", "6132710cfc13ae15b3000009", "6132710cfc13ae15b300000a", "6132710cfc13ae15b300000b", "6132710cfc13ae15b300000c", "6132710cfc13ae15b300000d", "6132710cfc13ae15b300000e", "6132710cfc13ae15b300000f"],
+            heats: [
+                {
+                    id: 0,
+                    riders: ["6132710cfc13ae15b3000001", "6132710cfc13ae15b3000002", "6132710cfc13ae15b3000003", "6132710cfc13ae15b3000004"],
+                    state: 'finished',
+                    results: [{
+                        riderId: "6132710cfc13ae15b3000001",
+                        color: 0,
+                        value: 23
+                    }, {
+                        riderId: "6132710cfc13ae15b3000002",
+                        color: 1,
+                        value: 12
+                    }, {
+                        riderId: "6132710cfc13ae15b3000003",
+                        color: 2,
+                        value: 32
+                    }, {
+                        riderId: "6132710cfc13ae15b3000004",
+                        color: 3,
+                        value: 31
+                    }]
+                }, {
+                    id: 1,
+                    riders: ["6132710cfc13ae15b3000005", "6132710cfc13ae15b3000006", "6132710cfc13ae15b3000007", "6132710cfc13ae15b3000008"],
+                    state: 'running',
+                    results: [{
+                        riderId: "6132710cfc13ae15b3000005",
+                        color: 0,
+                        value: 0
+                    }, {
+                        riderId: "6132710cfc13ae15b3000006",
+                        color: 1,
+                        value: 0
+                    }, {
+                        riderId: "6132710cfc13ae15b3000007",
+                        color: 2,
+                        value: 0
+                    }, {
+                        riderId: "6132710cfc13ae15b3000008",
+                        color: 3,
+                        value: 0
+                    }]
+                }, {
+                    id: 2,
+                    riders: ["6132710cfc13ae15b3000009", "6132710cfc13ae15b300000a", "6132710cfc13ae15b300000b"],
+                    state: 'idle',
+                    results: [{
+                        riderId: "6132710cfc13ae15b3000009",
+                        color: 0,
+                        value: 0
+                    }, {
+                        riderId: "6132710cfc13ae15b300000a",
+                        color: 1,
+                        value: 0
+                    }, {
+                        riderId: "6132710cfc13ae15b300000b",
+                        color: 2,
+                        value: 0
+                    }]
+                }
+            ]
+        },
+        {
+            id: 3,
             riders: ["6132710cfc13ae15b3000001", "6132710cfc13ae15b3000002", "6132710cfc13ae15b3000003", "6132710cfc13ae15b3000004", "6132710cfc13ae15b3000005", "6132710cfc13ae15b3000006", "6132710cfc13ae15b3000007", "6132710cfc13ae15b3000008", "6132710cfc13ae15b3000009", "6132710cfc13ae15b300000a", "6132710cfc13ae15b300000b", "6132710cfc13ae15b300000c", "6132710cfc13ae15b300000d", "6132710cfc13ae15b300000e", "6132710cfc13ae15b300000f"],
             heats: [
                 {

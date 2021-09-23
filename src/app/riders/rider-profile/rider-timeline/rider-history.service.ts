8a6479c30b0d332f1749d7c1b6c71f477fe30118
A.Messerli <messa4@bfh.ch>
2021-09-23 22:26:44 +0200

    Changed padding globally of the accordion (within timeline-container)

--- src/app/riders/rider-profile/rider-timeline/rider-history.service.ts
+++ src/app/riders/rider-profile/rider-timeline/rider-history.service.ts
@@ -17,7 +17,7 @@ export class RiderHistoryService {
     private eventTimeLines = new BehaviorSubject<EventTimeLine[]>([]);
 
     constructor() {
-        // ToDo: load initial data for currentRiderEvents and subscribe to websocket for updates
+        // ToDo: load initial data over api for data and subscribe to websocket for updates (only the currently ongoing timelines will be pushed over websocket)
         this.updateTimeLines(this.generateCurrentEventTimeLines());
     }
 
@@ -44,18 +44,19 @@ export class RiderHistoryService {
         this.eventTimeLines.next(currentEventTimeLines);
     }
 
+    // ToDo: Remove when api interface available
     private generateCurrentEventTimeLines(): EventTimeLine[] {
         const eventTimeLines: EventTimeLine[] = [];
         const riders = [
-            "6132710cfc13ae15b3000001",
-            "6132710cfc13ae15b3000002",
+            "6132710cfc13ae15b3000001", // pierette baltrushaitis -> has two ongoing timelines
+            "6132710cfc13ae15b3000002", // katerina humphries -> toggle on = two historical timelines
             "6132710cfc13ae15b3000003",
             "6132710cfc13ae15b3000004",
             "6132710cfc13ae15b3000005",
             "6132710cfc13ae15b3000006",
             "6132710cfc13ae15b3000007",
             "6132710cfc13ae15b3000008",
-            "6132710cfc13ae15b3000009",
+            "6132710cfc13ae15b3000009", // patrizia manna -> toggle off = only one historical timeline
             "6132710cfc13ae15b300000a",
             "6132710cfc13ae15b300000b",
             "6132710cfc13ae15b300000c",
@@ -63,6 +64,9 @@ export class RiderHistoryService {
             "6132710cfc13ae15b300000e",
             "6132710cfc13ae15b300000f"
         ];
+
+        let toggle = false;
+        let first = false;
 
         for (const rider of riders) {
             eventTimeLines.push({
@@ -72,11 +76,21 @@ export class RiderHistoryService {
                 riderId: rider,
                 timeline: [...DefaultTimeLineItemsOngoing]
             });
+            if(!first){
+                first = true;
+                eventTimeLines.push({
+                    id: Math.random().toString(),
+                    event: {...exampleEvent},
+                    ongoing: true,
+                    riderId: rider,
+                    timeline: [...DefaultTimeLineItemsOngoing]
+                });
+            }
             eventTimeLines.push({
                 id: Math.random().toString(),
                 event: {
                     ...exampleEvent,
-                    startDateTime: new Date(2020, 1, 12,0,0,0,0),
+                    startDateTime: new Date(2020, 1, 12, 0, 0, 0, 0),
                     id: 'riversurf-jam-thun-2020',
                     location: 'Thun'
                 },
@@ -84,18 +98,21 @@ export class RiderHistoryService {
                 riderId: rider,
                 timeline: [...DefaultTimeLineItemsLosing]
             });
-            eventTimeLines.push({
-                id: Math.random().toString(),
-                event: {
-                    ...exampleEvent,
-                    startDateTime: new Date(2020, 9, 31,0,0,0,0),
-                    id: 'riversurf-jam-halloween-2020',
-                    location: 'Schwarzenburg'
-                },
-                ongoing: false,
-                riderId: rider,
-                timeline: [...DefaultTimeLineItemsWinning]
-            });
+            if (toggle) {
+                eventTimeLines.push({
+                    id: Math.random().toString(),
+                    event: {
+                        ...exampleEvent,
+                        startDateTime: new Date(2020, 9, 31, 0, 0, 0, 0),
+                        id: 'riversurf-jam-halloween-2020',
+                        location: 'Schwarzenburg'
+                    },
+                    ongoing: false,
+                    riderId: rider,
+                    timeline: [...DefaultTimeLineItemsWinning]
+                });
+            }
+            toggle = !toggle;
         }
         return eventTimeLines;
     }

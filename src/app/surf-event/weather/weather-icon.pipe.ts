7d7507a441f270b9021c607b98ddd30c565ff630
Raphael Gerber <mail@raphaelgerber.com>
2021-09-29 23:29:51 +0200

    Added night icons and determine night or day made based on current time

--- src/app/surf-event/weather/weather-icon.pipe.ts
+++ src/app/surf-event/weather/weather-icon.pipe.ts
@@ -6,24 +6,27 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class WeatherIconPipe implements PipeTransform {
 
     transform(value: number): string {
+        const hours = new Date().getHours();
+        const dayOrNight = (hours > 6 && hours < 20) ? 'day' : 'night';
+
         switch (value) {
             default: return 'sunny';
 
-            case 1: return 'clear-day';
-            case 2: return 'partly-cloudy-day';
-            case 3: return 'overcast-day';
+            case 1: return `clear-${dayOrNight}`;
+            case 2: return `partly-cloudy-${dayOrNight}`;
+            case 3: return `overcast-${dayOrNight}`;
             case 4: return 'overcast';
-            case 5: return 'thunderstorms-day';
+            case 5: return `thunderstorms-${dayOrNight}`;
             case 6: return 'rain';
             case 7: return 'snow';
-            case 8: return 'fog-day';
+            case 8: return `fog-${dayOrNight}`;
             case 9: return 'sleet';
-            case 10: return 'partly-cloudy-day-rain';
+            case 10: return `partly-cloudy-${dayOrNight}-rain`;
             case 11: return 'drizzle';
-            case 12: return 'partly-cloudy-day-snow';
+            case 12: return `partly-cloudy-${dayOrNight}-snow`;
             case 13: return 'thunderstorms-rain';
             case 14: return 'mist';
-            case 15: return 'partly-cloudy-day-sleet';
+            case 15: return `partly-cloudy-${dayOrNight}-sleet`;
         }
     }
 }

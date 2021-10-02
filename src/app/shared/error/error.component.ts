376a6749d732122e8f146a14e1e8f05cf8123cc8
A.Messerli <messa4@bfh.ch>
2021-10-02 15:18:41 +0200

    Removed game section and added icon to failed resource

--- src/app/shared/error/error.component.ts
+++ src/app/shared/error/error.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterHistoryService} from "../../core/services/router-history.service";
 
 @Component({
@@ -6,9 +6,12 @@ import {RouterHistoryService} from "../../core/services/router-history.service";
     templateUrl: './error.component.html',
     styleUrls: ['./error.component.scss']
 })
-export class ErrorComponent implements OnInit, OnDestroy {
+export class ErrorComponent implements OnInit {
 
-    routerHistory: string[] = [];
+    routerHistory: {
+        url: string,
+        description: string
+    }[] = [];
 
     errorResource: string = '';
 
@@ -16,11 +19,27 @@ export class ErrorComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        this.routerHistory = this.routerHistoryService.getLastUrls();
+        const lastUrls = this.routerHistoryService.getLastUrls(6);
+        if (lastUrls.length > 0) {
+            this.routerHistory = lastUrls.map(x => {
+                const queryParamIndex = x.indexOf('?');
+                if (queryParamIndex !== -1) {
+                    x = x.substring(0, queryParamIndex);
+                }
+                const dest = x === '/' ? 'Home' : x.substring(1);
+                return {
+                    url: x,
+                    description: dest
+                }
+            });
+            if (this.routerHistory.length > 0 && this.routerHistory[0].description !== 'page-not-found') {
+                this.errorResource = this.routerHistory[0].description;
+            } else {
+                this.errorResource = '';
+            }
+        } else {
+            this.routerHistory = [];
+            this.errorResource = '';
+        }
     }
-
-    ngOnDestroy() {
-
-    }
-
 }
